07396d0fc928b9c67c4b675d5657fbeef0b0d7c7
Brisa Aylen Brussa <bbrussa@frba.utn.edu.ar>
2024-06-05 22:54:07 -0300

    dado por el enunciado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,17 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+---Abstracciones dadas por el enunciado
+type Palabra = String
+type Verso = String
+type Estrofa = [Verso]
+type Artista = String -- Solamente interesa el nombre
+
+esVocal :: Char -> Bool
+esVocal = flip elem "aeiou"
+
+tieneTilde :: Char -> Bool
+tieneTilde = flip elem "áéíóú"
+
+cumplen :: (a -> b) -> (b -> b -> Bool) -> a -> a -> Bool
+cumplen f comp v1 v2 = comp (f v1) (f v2)
